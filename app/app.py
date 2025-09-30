2874b606dde343b8e21ce1baf0ca6d8a63f83db6
Andrea Mentasti <andrea.mentasti@wyssacademy.org>
2025-09-30 11:20:42 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -635,7 +635,7 @@ Analyze it in the context of US political discourse on climate change and respon
             how_to=[
             "After the design of the prompts, it's time to run the code to query OpenAI GPT 4o-mini model to perform the task of interest. For this phase it's crucial to have the following two as inputs:",
             "1) A dataset with observation id, text snippets. Other variables will be discarded before running the main functions.",
-            "2) The two prompts, one for each task for GPT model.",
+            "2) The two prompts, one for each task for GPT model."
         
             ],
             ask_yourself=[
