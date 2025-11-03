cad68c0fa5766437ac15f0308e147e277c7f39e2
Andrea Mentasti <andrea.mentasti@wyssacademy.org>
2025-11-03 11:25:59 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -483,11 +483,33 @@ Analyze it in the context of US political discourse on climate change and respon
 
             blurb_md=(
                 "This is the final step of the pipeline. You will now run the prediction code that applies your prompts to your dataset. "
-                "This step applies the model instructions finalized in Step 4 to your text data, producing the full narrative prediction.\n\n"
+                "This page of the guide will help you adapting the scripts that we provide in the Github repository to your objective. "
+                "In practice, this step is where you will apply the model instructions finalized in Step 4 to your text data, producing the full narrative prediction.\n\n"
                 "##### Step-by-Step instructions:\n"
-                "1) Download the script ````annotation_openai_stage2```` (or ````annotation_openai_stage1```` for relevance classification) from the `code and prompts/`folder in the  [GitHub repository](https://github.com/AndreaMentasti/Political-Narratives-Package/tree/main).\n\n"
-                "2) Set your Python environment (e.g., activate `Political_Narratives`) and store your OpenAI API Key as an environment variable. We suggest to follow the steps [here](https://github.com/AndreaMentasti/Political-Narratives-Package/tree/main) if you are not familiar with this.\n\n"
-                "3) Set the folder structure properly (you can check the folder structure in the Github repository).\n\n"
+                "1) **(For beginner Python users)** The first step is to download Python at the [link](https://www.python.org/downloads/).\n\n"
+                "2) **(For beginner Python users)** Then, we suggest you to download Anaconda and Anaconda Prompt at the [link](https://www.anaconda.com/docs/getting-started/anaconda/install).\n\n"
+                "3) Download the files in ````code and prompts```` and the ````environment.yml````. Then, you must build the following folder structure locally:\n\n"
+                st.markdown(
+                    """
+                ```bash
+                <main>/
+                ├─ code/
+                │  ├─ annotation_openai_stage1.py
+                │  ├─ annotation_openai_stage2.py
+                │  └─ prompts/
+                │     ├─ system_message_stage1.json        # <-- your input message
+                │     └─ system_message_stage2.json        # <-- your input message
+                ├─ data/
+                │  └─ output/
+                │     ├─ your_data_stage1.csv              # <-- your input CSV
+                │     └─ your_data_stage2.csv              # <-- your input CSV
+                ├─ output/
+                │  └─ data/
+                │     ├─ openai_output/
+                │     └─ openai_final/
+                └─ logs/
+                └─ environment.yml                         # <-- your environment requirements
+                
                 "4) Make sure your finalized prompt files (`system_message_stage1.json` and `system_message_stage2.json`) are saved in the correct folder, as the scripts will load them automatically.\n\n"
                 "4) Make minimal changes in the code to match your path, the characters defined in the prompt, and the dataset. If you want to perform relevance classification, you just need to change the path.\n\n"
                 "All the steps are explained in detail [here](https://github.com/AndreaMentasti/Political-Narratives-Package/tree/main). Check the detailed GitHub instructions if you need guidance."
