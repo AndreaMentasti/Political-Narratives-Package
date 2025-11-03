feebbdc98ac07c079109b87993bbe93cb773045e
Andrea Mentasti <andrea.mentasti@wyssacademy.org>
2025-11-03 11:39:36 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -481,14 +481,14 @@ Analyze it in the context of US political discourse on climate change and respon
             ],
             key_prefix="s5_outputs",
 
-            blurb_md=(
-                "This is the final step of the pipeline. You will now run the prediction code that applies your prompts to your dataset. "
-                "This page of the guide will help you adapting the scripts that we provide in the Github repository to your objective. "
-                "In practice, this step is where you will apply the model instructions finalized in Step 4 to your text data, producing the full narrative prediction.\n\n"
-                "##### Step-by-Step instructions:\n"
-                "1) **(For beginner Python users)** The first step is to download Python at the [link](https://www.python.org/downloads/).\n\n"
-                "2) **(For beginner Python users)** Then, we suggest you to download Anaconda and Anaconda Prompt at the [link](https://www.anaconda.com/docs/getting-started/anaconda/install).\n\n"
-                "3) Download the files in *code and prompts* and the *environment.yml*. Then, build the following folder structure locally:\n\n"
+            blurb_md==r"""
+                This is the final step of the pipeline. You will now run the prediction code that applies your prompts to your dataset. 
+                This page of the guide will help you adapting the scripts that we provide in the Github repository to your objective. 
+                In practice, this step is where you will apply the model instructions finalized in Step 4 to your text data, producing the full narrative prediction.\n\n
+                ##### Step-by-Step instructions:\n
+                1) **(For beginner Python users)** The first step is to download Python at the [link](https://www.python.org/downloads/).\n\n
+                2) **(For beginner Python users)** Then, we suggest you to download Anaconda and Anaconda Prompt at the [link](https://www.anaconda.com/docs/getting-started/anaconda/install).\n\n
+                3) Download the files in *code and prompts* and the *environment.yml*. Then, build the following folder structure locally:\n\n
                 ```bash
                 <main>/
                 ├─ code/
@@ -507,9 +507,9 @@ Analyze it in the context of US political discourse on climate change and respon
                 │     └─ openai_final/
                 └─ logs/
                 └─ environment.yml                         # <-- your environment requirement
-                "4) Make sure your finalized prompt files (`system_message_stage1.json` and `system_message_stage2.json`) are saved in the correct folder, as the scripts will load them automatically.\n\n"
-                "4) Make minimal changes in the code to match your path, the characters defined in the prompt, and the dataset. If you want to perform relevance classification, you just need to change the path.\n\n"
-                "All the steps are explained in detail [here](https://github.com/AndreaMentasti/Political-Narratives-Package/tree/main). Check the detailed GitHub instructions if you need guidance."
+                4) Make sure your finalized prompt files (`system_message_stage1.json` and `system_message_stage2.json`) are saved in the correct folder, as the scripts will load them automatically.\n\n
+                4) Make minimal changes in the code to match your path, the characters defined in the prompt, and the dataset. If you want to perform relevance classification, you just need to change the path.\n\n
+                All the steps are explained in detail [here](https://github.com/AndreaMentasti/Political-Narratives-Package/tree/main). Check the detailed GitHub instructions if you need guidance."""
                 
             ),
         )
